dc8976c07fc875e475409204f39ebd5ba4bf0001
axel <axel.chacon@tecsup.edu.pe>
2023-05-31 19:13:22 -0500

    Add project files.

---
+++ lab09/lab09/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using lab09.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab09
+{
+    public partial class App : Application
+    {
+
+        public static float ScreenHeight { get; set; }
+        public static float ScreenWidth { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SwipeDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
